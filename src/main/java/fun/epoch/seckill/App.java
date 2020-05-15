23abb07cdd1ae0ae431db4c76305e91aada25a81
epochwz <epoch.wz@gmail.com>
2020-05-15 08:14:00 +0800

    开启 Spring Data JPA 的 自动扫描

--- src/main/java/fun/epoch/seckill/App.java
+++ src/main/java/fun/epoch/seckill/App.java
@@ -2,7 +2,9 @@ package fun.epoch.seckill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
